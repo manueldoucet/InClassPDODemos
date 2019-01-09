2bfd47a5c990909c5ba19e768c3c5d92d26a30a2
manueldoucet <44036557+manueldoucet@users.noreply.github.com>
2019-01-09 13:12:03 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-<script type="text/javascript"  <script type="text/javascript" src="https://raw.githubusercontent.com/manueldoucet/InClassPDODemos/master/main.js"></script>
+<script type="text/javascript"  <script type="text/javascript" src=" <script src='https://raw.githubusercontent.com/manueldoucet/InClassPDODemos/e7debb208b88b3c2a52082edc3ce4abad2100ada/main.js' type='text/javascript'/>"></script>
 <script type="text/javascript">
    $(function(){
       alert("My First Jquery Test");
